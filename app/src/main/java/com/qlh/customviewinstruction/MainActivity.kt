453d2680deadfec65d9005264234f520b060aea5
qlh <708937822@qq.com>
2018-09-30 10:10:53 +0800

    优化RangeBarView
    Signed-off-by: qlh <708937822@qq.com>

--- app/src/main/java/com/qlh/customviewinstruction/MainActivity.kt
+++ app/src/main/java/com/qlh/customviewinstruction/MainActivity.kt
@@ -2,11 +2,18 @@ package com.qlh.customviewinstruction
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        view_range_bar.setRectLineHeight(10)
+        view_range_bar.setDialogToProgressSpace(50)
+        view_range_bar.setRangeTextToProgressSpace(100)
+        view_range_bar.setDialogColor(resources.getColor(R.color.blue_275D9D))
+        view_range_bar.setDialogWidth(100)
+        view_range_bar.setDialogCornerRadius(20)
     }
 }
